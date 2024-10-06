fc3f67843d87efce873189d031e53b5d1b4e1238
Sahil Kulria <sahilkulria002@gmail.com>
2024-10-06 18:34:12 +0530

    working py_with_accelstepper

--- py_with_accelstepper.py
+++ py_with_accelstepper.py
@@ -2,8 +2,8 @@ import serial
 import time
 import two_D_UI
 
+# this connects to pyserial_communication in arduino
 
-# Replace 'COM3' with your Arduino's serial port (check in Arduino IDE under Tools > Port)
 arduino = serial.Serial('COM5', 9600, timeout=1)
 time.sleep(2)  # Wait for the connection to establish
 
@@ -25,15 +25,6 @@ def send_commands_to_arduino(commands):
     arduino.write((command_str + '\n').encode())  # Send command to Arduino
     time.sleep(0.1)  # Optional delay after sending commands
 
-# commands = [
-#     [50, 100, -25],   # Move motor A 50 steps, motor B 100 steps, motor C -25 steps
-    # [30, 50, 0],      # Move motor A 30 steps, motor B 50 steps, motor C 0 steps
-    # [0, -75, 20]      # Move motor A 0 steps, motor B -75 steps, motor C 20 steps
-# ]
-
-# 10 20 0
-# -10 -20 0
-
 try:
 
     while True:
@@ -50,32 +41,13 @@ try:
             break
         else:
             
-            while True :
+            while user_input != "q" :
+                commands = []
+                step_list = two_D_UI.run_ui()
+                for steps in step_list :
+                    commands.append([-steps[1], 0, steps[0]])
+                send_commands_to_arduino(commands)
                 user_input = input("give e to stop or 3 ints, one for each motor : ")
-                if user_input == "e" :
-                    break
-                else :
-                    commands = []
-                    step_list = two_D_UI.run_ui()
-                    for steps in step_list :
-                        commands.append([-steps[1], 0, steps[0]])
-                    send_commands_to_arduino(commands)
-                # user_input = input("give e to stop or 3 ints, one for each motor : ")
-                # if user_input == "e" :
-                #     print(commands)
-                #     send_commands_to_arduino(commands)
-                #     break
-                # else : 
-                #     steps = list(map(int, user_input.split()))
-                    
-                #     if len(steps) != 3:
-                #         print("Please enter exactly three integers.")
-                #         continue
-                    
-                #     # Append the steps to the commands list
-                #     commands.append(steps)
-                #     print(f"Command added: {steps}")
-
 
 except KeyboardInterrupt:
     print("\nProgram interrupted.")
@@ -84,12 +56,3 @@ finally:
     # Always close the serial connection when done
     arduino.close()
     print("Serial connection closed.")
-
-
-
-# control_led("on")  # Turn on the LED
-# time.sleep(5)
-# control_led("off")  # Turn off the LED
-
-# # Don't forget to close the connection when done
-# arduino.close()
